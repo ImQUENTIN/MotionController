78eb07638d51ba06eb490f1080a078fe63c49c94
JinZishu <jin963.123@qq.com>
2017-03-08 21:38:12 +0800

    send

--- MotionController/src/Communication/dsp-arm/senddata.c
+++ MotionController/src/Communication/dsp-arm/senddata.c
@@ -6,13 +6,14 @@ ERROR_CODE senddata(word cmd,word mark,word *dat_buf, word dat_len)
 {
 	int str[COMMUNICATION_MAX_LEN];
 	str[0] = 0x23;
+	str[1] = dat_len+2;
 	str[2] = cmd;
 	str[3] = mark;
 
-	if(dat_len > 0 || dat_len <= COMMUNICATION_MAX_LEN)
+	if(dat_len > 0 && dat_len <= COMMUNICATION_MAX_LEN)
 	{
 		int i, j;
-		str[1] = dat_len;
+
 		str[dat_len + 4] = 0;
 		memcpy(str+4, &dat_buf, dat_len);
 		for(i = 0; i < dat_len; i++)
